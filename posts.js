0fa2111409e50265629afeac3c7024ef1217884c
harrysignups12-pixel <harrysignups12@gmail.com>
2025-08-31 18:30:25 +0200

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,18 @@
+// posts.js
+const posts = [
+  {
+    title: "My First Blog Post",
+    filename: "blog1.html",
+    preview: "Hello world! 🎉 This is my very first blog post."
+  },
+  {
+    title: "Learning CSS",
+    filename: "blog2.html",
+    preview: "Today I styled my blog with CSS and it looks awesome!"
+  },
+  {
+    title: "Adding JavaScript",
+    filename: "blog3.html",
+    preview: "Now my site can generate posts automatically."
+  }
+];
